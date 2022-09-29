4902b3ca6062922d15cd25caaf264fc3e39bfcd3
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2022-09-29 15:14:45 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,63 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { actionGetEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleClick = (email) => {
+    const { getEmail, history } = this.props;
+    getEmail(email);
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    const regExpEmail = /\S+[@]\w+[.]\w+/gm;
+    const minLength = 6;
+    const cond = (regExpEmail.test(email) && password.length >= minLength);
+    return (
+      <div>
+        <input
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ ({ target: { value } }) => this.setState({ email: value }) }
+        />
+
+        <input
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ ({ target: { value } }) => this.setState({ password: value }) }
+        />
+
+        <button
+          type="button"
+          disabled={ !cond }
+          onClick={ () => this.handleClick(email) }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  getEmail: (email) => dispatch(actionGetEmail(email)),
+});
+
+Login.propTypes = {
+  getEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,10 @@
 // Coloque aqui suas actions
+
+// Actions Types
+
+export const GET_EMAIL = 'GET_EMAIl';
+
+export const actionGetEmail = (email) => ({
+  type: GET_EMAIL,
+  payload: email,
+});
